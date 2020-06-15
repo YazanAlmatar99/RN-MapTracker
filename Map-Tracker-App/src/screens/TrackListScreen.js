579b2e7dd08705241130acda78ecb6b3117781a7
Yazan <yazanmatar90@gmail.com>
2020-06-15 19:22:04 -0400

    buttons to test navigation

--- Map-Tracker-App/src/screens/TrackListScreen.js
+++ Map-Tracker-App/src/screens/TrackListScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const TrackListScreen = () => {
+const TrackListScreen = (props) => {
   return (
     <View>
       <Text>TrackListScreen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => props.navigation.navigate("TrackDetail")}
+      />
     </View>
   );
 };
