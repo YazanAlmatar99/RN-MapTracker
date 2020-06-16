8860b710e70fd9c993ba2b752c8e5f71f15d9303
Yazan <yazanmatar90@gmail.com>
2020-06-16 03:06:10 -0400

    added sign up function

--- Map-Tracker-App/src/context/AuthContext.js
+++ Map-Tracker-App/src/context/AuthContext.js
@@ -1,13 +1,44 @@
 import createDataContext from "./createDataContext";
+import AsyncStorage from "@react-native-community/async-storage";
+import trackerAPI from "../API/tracker";
+import { navigate } from "../navigationRef";
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
 };
+const signup = (dispatch) => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerAPI.post("/signup", {
+        email: email,
+        password: password,
+      });
+      await AsyncStorage.setItem("token", response.data.token);
+      dispatch({ type: "signup", payload: response.data.token });
+      navigate("TrackList");
+    } catch (err) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong, please try again.",
+      });
+    }
+  };
+};
+const signin = (dispatch) => {
+  return ({ email, password }) => {};
+};
+const signout = (dispatch) => {
+  return () => {};
+};
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signin, signout, signup },
+  { token: null, errorMessage: "" }
 );
