42ecc674d9501499e9a0848d2821421963e888cd
Yazan <yazanmatar90@gmail.com>
2020-06-15 20:16:04 -0400

    sign up screen styles and props

--- Map-Tracker-App/src/screens/SignupScreen.js
+++ Map-Tracker-App/src/screens/SignupScreen.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
-      <Input label="Password" />
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+      />
       <Spacer>
         <Button
           title="Sign Up"
           onPress={() => props.navigation.navigate("Signin")}
         />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 export default SignupScreen;
